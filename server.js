427600d5f5d19b107fab6e71e6ed2f8a07bf4192
junhei <oonjunhei@gmail.com>
2025-11-06 14:17:40 +0800

    dashboard Commit Message

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Test route
+
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from Express!' });
 });
