3b911e3b21cbc2d43e0221991f2dcb3c90d37d41
junhei <oonjunhei@gmail.com>
2025-11-07 10:26:14 +0800

    Dashboard Refined

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import GlobalHeader from './GlobalHeader';
-import Sidebar from './Sidebar';
-import Dashboard from './Dashboard';
-import './Dashboard.css';
+import Sidebar from './Sidebar/Sidebar';       
+import Dashboard from './Dashboard/Dashboard';   
+import './Dashboard/Dashboard.css'; 
 
 function App() {
   return (
